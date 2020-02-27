09756a6e3638ee9c08cbe45a982c9d25475f317a
Warkdev <cedric.servais@outlook.com>
2020-02-27 21:05:02 +0100

    Adding tooltip to the minimap icon.

--- modules/minimap.lua
+++ modules/minimap.lua
@@ -2,15 +2,27 @@
 local icon;
 local launcher;
 
-function MinimapButtonOnClick()    
-    ShowManager();
+function MinimapButtonOnClick(self, button)    
+    if button == "RightButton" then
+        OpenConfigPane();
+    elseif button == "LeftButton" then
+        ShowManager();
+    end    
+end
+
+function MinimapTooltipShow(tooltip)
+    tooltip:AddLine("BestInSlot-Classic");
+    tooltip:AddLine("Version: "..VERSION);
+    tooltip:AddLine("Left-Click: Open the main window");
+    tooltip:AddLine("Right-Click: Open the settings window");
 end
 
 function CreateMinimapIcon()
     launcher = LibStub("LibDataBroker-1.1"):NewDataObject("BestInSlotClassic", {
         type = "launcher",
         icon = "Interface\\Addons\\BestInSlotClassic\\assets\\logo_32x32",    
-        OnClick = MinimapButtonOnClick   
+        OnClick = MinimapButtonOnClick,
+        OnTooltipShow = MinimapTooltipShow,
     });
     
     icon = LibStub("LibDBIcon-1.0");
